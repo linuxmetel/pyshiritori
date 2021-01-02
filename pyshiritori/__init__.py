d8b641de1b08120ccb4294f7a6a5817603d5d9e0
Linuxmetel <linuxmetel@yandex.com>
2021-01-02 12:20:54 +0900

    1.0.1

---
+++ pyshiritori/__init__.py
@@ -0,0 +1,8 @@
+from .pyshiritori import *
+
+__copyright__    = 'Copyright (C) 2020 Linuxmetel & Lithops'
+__version__      = '0.0.1'
+__license__      = 'GPLv3'
+__author__       = 'Linuxmetel & Lithops'
+__author_email__ = 'linuxmetel@yandex.com'
+__url__          = 'http://github.com/linuxmetel/pyshiritori'
